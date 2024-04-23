b94cc2888e568483813eac22764e0ed9e50d10ff
Amariah <amariah.abish@gmail.com>
2024-04-23 11:06:06 +0300

    Full Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar"; // Importing NavBar component
+import Home from "./Home"; // Importing Home component
+import About from "./About"; // Importing About component
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar /> {/* Render NavBar component */}
+      <Home /> {/* Render Home component */}
+      <About /> {/* Render About component */}
     </div>
   );
 }
